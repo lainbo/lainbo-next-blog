f8a77ff3f1a639aed9d903ce5036c9d42a65abc3
liangjiacheng <lainbo@foxmail.com>
2023-07-11 16:47:33 +0800

    chore: 🔧 变更配置

--- themes/next/components/SideAreaLeft.js
+++ themes/next/components/SideAreaLeft.js
@@ -56,7 +56,7 @@ const SideAreaLeft = props => {
                             <div className='mt-2 text-center dark:text-gray-300 font-light text-xs'>
                                 <span className='px-1 '>
                                     <strong className='font-medium'>{postCount}</strong>{locale.COMMON.POSTS}</span>
-                                <span className='px-1 busuanzi_container_site_uv !hidden'>
+                                <span className='px-1 busuanzi_container_site_uv hidden'>
                                     | <strong className='pl-1 busuanzi_value_site_uv font-medium' />{locale.COMMON.VISITORS}</span>
                                 {/* <span className='px-1 busuanzi_container_site_pv hidden'>
                 | <strong className='pl-1 busuanzi_value_site_pv font-medium'/>{locale.COMMON.VIEWS}</span> */}
