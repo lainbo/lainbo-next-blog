1fc5ca97c04ee720805669e18a4dd1de9fb87aba
Jason Liang <lainbo@foxmail.com>
2023-12-24 02:02:11 +0800

    Update BlogPostCard styling

--- themes/gitbook/components/BlogPostCard.js
+++ themes/gitbook/components/BlogPostCard.js
@@ -6,7 +6,7 @@ const BlogPostCard = ({ post, className }) => {
   const router = useRouter()
   const currentSelected = router.asPath.split('?')[0] === '/' + post.slug
   return (
-        <div key={post.id} className={`${className} py-1 cursor-pointer px-2 border border-transparent hover:bg-gray-50 rounded-md dark:hover:bg-gray-600  ${currentSelected ? 'bg-[#e8ffea] text-[#00b42a] !border-[#aff0b5] dark:bg-[#27c34633] dark:text-[#27c346] dark:!border-[#484849]' : ''}`}>
+        <div key={post.id} className={`${className} py-1 cursor-pointer px-2 border border-transparent hover:bg-gray-50 rounded-md dark:hover:bg-gray-600  ${currentSelected ? '!bg-[#e8ffea] text-[#00b42a] !border-[#aff0b5] dark:!bg-[#27c34633] dark:text-[#27c346] dark:!border-[#484849]' : ''}`}>
             <div className="flex flex-col w-full select-none">
                 <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref>
                     {post.title}
