bfd631656ffd45664a1835bf4a234f483a09f168
Jason Liang <lainbo@foxmail.com>
2024-01-17 13:19:53 +0800

    Update website link in .env.local and blog.config.js

--- lainbo-blog-manager.sh
+++ lainbo-blog-manager.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+function rebuild_and_start {
+    echo "正在重新构建并启动 lainbo-blog..."
+    yarn build
+    pm2 start yarn --name "lainbo-blog" -- start
+}
+
+function stop_and_delete {
+    pm2 stop lainbo-blog
+    pm2 delete lainbo-blog
+}
+
 echo "请选择要执行的操作："
 echo "1) 重新构建并启动 lainbo-blog"
 echo "2) 拉取代码，构建并启动 lainbo-blog"
@@ -8,22 +19,23 @@ read -p "输入选择（1/2/3）: " choice
 
 cd /home/lainbo-next-blog
 
+# 检查 lainbo-blog 是否在运行
+pm2 describe lainbo-blog > /dev/null
+RUNNING=$?
+
 case $choice in
   1)
-    echo "正在重新构建并启动 lainbo-blog..."
-    yarn build
-    pm2 restart lainbo-blog || pm2 start yarn --name "lainbo-blog" -- start
+    [ $RUNNING -eq 0 ] && stop_and_delete
+    rebuild_and_start
     ;;
   2)
-    echo "正在拉取代码，构建并启动 lainbo-blog..."
+    echo "正在拉取代码..."
     git pull
-    yarn build
-    pm2 restart lainbo-blog || pm2 start yarn --name "lainbo-blog" -- start
+    [ $RUNNING -eq 0 ] && stop_and_delete
+    rebuild_and_start
     ;;
   3)
-    echo "正在停止并删除 lainbo-blog..."
-    pm2 stop lainbo-blog
-    pm2 delete lainbo-blog
+    [ $RUNNING -eq 0 ] && stop_and_delete || echo "lainbo-blog 不在运行。"
     ;;
   *)
     echo "无效的选择。"
