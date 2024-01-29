c1ce0033ec464140403eab3d9a168451f27758c9
Jason Liang <lainbo@foxmail.com>
2024-01-29 20:26:19 +0800

    Remove unused font from blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -51,7 +51,6 @@ const BLOG = {
     'https://s1.hdslb.com/bfs/static/jinkela/long/font/regular.css',
     'https://fonts.loli.net/css2?family=Inter:wght@450&display=swap',
     'https://fonts.loli.net/css2?family=JetBrains+Mono&display=swap',
-    'https://fonts.loli.net/css2?family=Noto+Serif+SC&display=swap'
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
