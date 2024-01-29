c7c64a02a5e7f4001d8a4ffe843cff74e7d7f395
Jason Liang <lainbo@foxmail.com>
2024-01-29 10:03:43 +0800

    Refactor BlogPostCard component

--- themes/gitbook/components/BlogPostCard.js
+++ themes/gitbook/components/BlogPostCard.js
@@ -6,9 +6,9 @@ const BlogPostCard = ({ post, className }) => {
   const router = useRouter()
   const currentSelected = router.asPath.split('?')[0] === '/' + post.slug
   return (
-        <div key={post.id} className={`${className} py-1 cursor-pointer px-2 border border-transparent hover:bg-[#0000000f] rounded-md dark:hover:bg-[#ffffff1f]  ${currentSelected ? '!bg-[#e8ffea] text-[#00b42a] !border-[#aff0b5] dark:!bg-[#27c34633] dark:text-[#27c346] dark:!border-[#484849]' : ''}`}>
+        <div key={post.id} className={`${className} cursor-pointer border border-transparent hover:bg-[#0000000f] rounded-md dark:hover:bg-[#ffffff1f]  ${currentSelected ? '!bg-[#e8ffea] text-[#00b42a] !border-[#aff0b5] dark:!bg-[#27c34633] dark:text-[#27c346] dark:!border-[#484849]' : ''}`}>
             <div className="flex flex-col w-full select-none">
-                <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref>
+                <Link className="py-1 px-2" href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref>
                     {post.title}
                 </Link>
             </div>
