069a37c293e60556cce7b74cde286f4088e2e432
Jason Liang <lainbo@foxmail.com>
2024-02-01 20:53:23 +0800

    Refactor BlogPostCard component styling

--- themes/gitbook/components/BlogPostCard.js
+++ themes/gitbook/components/BlogPostCard.js
@@ -6,12 +6,13 @@ const BlogPostCard = ({ post, className }) => {
   const router = useRouter()
   const currentSelected = router.asPath.split('?')[0] === '/' + post.slug
   return (
-        <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref> <div key={post.id} className={`${className} py-1.5 cursor-pointer px-1.5 hover:bg-gray-50 rounded-md dark:hover:bg-gray-600  ${currentSelected ? 'bg-green-50 text-green-500' : ''}`}>
-            <div className="flex flex-col w-full select-none">
-                {post.title}
-            </div>
+    <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref>
+      <div key={post.id} className={`${className} py-1.5 cursor-pointer px-1.5 transition-all border border-transparent hover:bg-[#0000000f] dark:hover:bg-[#ffffff1f] rounded-md ${currentSelected ? '!bg-[#e6f4ff] text-[#0958d9] !border-[#91caff] dark:!bg-[#111a2c] dark:text-[#3c89e8] dark:!border-[#15417e]' : ''}`}>
+        <div className="flex flex-col w-full select-none">
+          {post.title}
         </div>
-        </Link>
+    </div>
+    </Link>
   )
 }
 
