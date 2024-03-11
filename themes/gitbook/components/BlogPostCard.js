21d38ddef2594cfadccf7b5be3488dd50d65ac7e
Jason Liang <lainbo@foxmail.com>
2024-03-11 10:18:19 +0800

    Add GitBook latest post red badge

--- themes/gitbook/components/BlogPostCard.js
+++ themes/gitbook/components/BlogPostCard.js
@@ -11,14 +11,14 @@ const BlogPostCard = ({ post, className }) => {
   const currentSelected = router.asPath.split('?')[0] === '/' + post.slug
   const url = checkContainHttp(post.slug) ? sliceUrlFromHttp(post.slug) : `${siteConfig('SUB_PATH', '')}/${post.slug}`
   return (
-        <Link href={url} passHref> <div key={post.id} className={`${className} relative py-1.5 cursor-pointer px-1.5 hover:bg-gray-50 rounded-md dark:hover:bg-gray-600  ${currentSelected ? 'bg-green-50 text-green-500' : ''}`}>
-            <div className="w-full select-none">
-                <NotionIcon icon={post?.pageIcon}/> {post.title}
+    <Link href={url} passHref> <div key={post.id} className={`${className} relative py-1.5 cursor-pointer px-1.5 hover:bg-gray-50 rounded-md dark:hover:bg-gray-600  ${currentSelected ? 'bg-green-50 text-green-500' : ''}`}>
+      <div className="w-full select-none">
+        <NotionIcon icon={post?.pageIcon} /> {post.title}
       </div>
       {/* 最新文章加个红点 */}
-      {post?.isLatest && siteConfig('GITBOOK_LATEST_POST_RED_BADGE', false, CONFIG) && <Badge/>}
-        </div>
-        </Link>
+      {post?.isLatest && siteConfig('GITBOOK_LATEST_POST_RED_BADGE', false, CONFIG) && <Badge />}
+    </div>
+    </Link>
   )
 }
 
